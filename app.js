dc06ef25f5cb42e52d46447d8455954a4cefaea9
erfangumelar <erfangumelar@gmail.com>
2022-10-22 21:24:35 +0700

    Init setup awal

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import apiRouter from "./routes/api.js";
+
+const app = express();
+
+// mengirimkan request dalam bentuk json
+app.use(express.json());
+
+// mengirimkan request dalam bentuk x-www-form-encoded (method post)
+app.use(express.urlencoded({ extended: true }));
+
+// router
+app.use("/", apiRouter);
+
+// handle 404 error
+app.use((req, res) => {
+  res.status(404).json({
+    message: "404_NOT_FOUND",
+  });
+});
+
+// setting port
+app.listen(3000, () => {
+  console.log(`Server started on 3000`);
+});
